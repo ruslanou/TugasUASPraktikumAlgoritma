93bcc9d443f761301aa38745550391829c194c0c
Ruslanou <aruslan8395@gmail.com>
2019-05-26 01:00:28 +0700

    keluar dan save file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main(){
-    cout << setw(8);
     cout << "Kumpulan Latihan & Tugas Akhir Praktikum Algoritma" << endl;
-    cout << "\t++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
     
     
     
