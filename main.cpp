03dba5226d24e3a262da1c720e1504096689eabc
Ruslanou <aruslan8395@gmail.com>
2019-05-26 01:16:34 +0700

    menambahkan daftar list modul pertemuan

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main(){
     cout << "Kumpulan Latihan & Tugas Akhir Praktikum Algoritma" << endl;
     cout << "++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << endl;
+    
+    cout << "DAFTAR MODUL PERTEMUAN PRAKTIKUM ALGORITMA C++: " << endl;
+    cout << "(1). DASAR PEMROGRAMAN C++\t(7). ARRAY" << endl;
+    cout << "(2). DECISION <IF>\t\t(8). STRING" << endl;
+    cout << "(3). DECISION <SWITCH>\t\t(9). POINTER" << endl;
+    cout << "(4). LOOPING <FOR>\t\t(10). STRUCTURE" << endl;
+    cout << "(5). LOOPING <WHILE>\t\t(11). SORTING" << endl;
+    cout << "(6). FUNCTION\t\t\t(12). SEARCHING" << endl;
+    
     
     
     
